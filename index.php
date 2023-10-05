fbaad6ea1bb9216cea493673c277e613e80a83e6
Abu Bakar Siddique <mohammad.akash31@gmail.com>
2023-10-06 00:11:04 +0600

    Set header & footer through funtions

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
     require_once('functions\function.php');
     get_header();
 ?>
-    <!-- Navbar End -->
-
-
     <!-- Carousel Start -->
     <div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="owl-carousel header-carousel position-relative">
@@ -560,8 +557,6 @@
             </div>
         </div>
     </div>
-    <!-- Testimonial End -->
-
 <?php
 get_footer();
 ?>
