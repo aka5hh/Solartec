f38b87c7fec035f84fc165789dbd27ec2bce427c
Abu Bakar Siddique <mohammad.akash31@gmail.com>
2023-10-12 11:26:44 +0600

    Fixed active header

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,11 @@
+<?php
+// print_r($_SERVER);
+// echo $_SERVER['PHP_SELF'];
+$convert=explode('/',$_SERVER['PHP_SELF']);
+// print_r($convert);
+$page=$convert[3];
+//echo $page;
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -59,22 +67,13 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.php" class="nav-item nav-link active">Home</a>
-                <a href="about.php" class="nav-item nav-link">About</a>
-                <a href="service.php" class="nav-item nav-link">Service</a>
-                <a href="project.php" class="nav-item nav-link">Project</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                    <div class="dropdown-menu bg-light m-0">
-                        <a href="feature.php" class="dropdown-item">Feature</a>
-                        <a href="quote.php" class="dropdown-item">Free Quote</a>
-                        <a href="team.php" class="dropdown-item">Our Team</a>
-                        <a href="testimonial.php" class="dropdown-item">Testimonial</a>
-                        <a href="404.php" class="dropdown-item">404 Page</a>
-                    </div>
-                </div>
-                <a href="contact.php" class="nav-item nav-link">Contact</a>
+                <a href="index.php" class="nav-item nav-link <?php if($page=='index.php') {echo 'active';}?>">Home</a>
+                <a href="about.php" class="nav-item nav-link <?php if($page=='about.php') {echo 'active';}?>">About</a>
+                <a href="service.php" class="nav-item nav-link <?php if($page=='service.php') {echo 'active';}?>">Service</a>
+                <a href="project.php" class="nav-item nav-link <?php if($page=='project.php') {echo 'active';}?>">Project</a>
+                <a href="team.php" class="nav-item nav-link <?php if($page=='team.php') {echo 'active';}?>">Team</a>
+                <a href="contact.php" class="nav-item nav-link <?php if($page=='contact.php') {echo 'active';}?>">Contact</a>
             </div>
-            <a href="" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Get A Quote<i class="fa fa-arrow-right ms-3"></i></a>
+            <a href="quote.php" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block <?php if($page=='quote.php') {echo 'active';}?> ">Get A Quote<i class="fa fa-arrow-right ms-3"></i></a>
         </div>
     </nav>
